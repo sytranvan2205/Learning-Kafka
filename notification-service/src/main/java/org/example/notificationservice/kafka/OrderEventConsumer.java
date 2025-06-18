3a9c7ab949abd287a28d87ef74940beacdb5ecfb
IMT-SOFT\sytran <sytran@imt-soft.com>
2025-06-18 09:39:17 +0700

    push code service notify

---
+++ notification-service/src/main/java/org/example/notificationservice/kafka/OrderEventConsumer.java
@@ -0,0 +1,9 @@
+package org.example.notificationservice.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderEventConsumer {
+
+}
