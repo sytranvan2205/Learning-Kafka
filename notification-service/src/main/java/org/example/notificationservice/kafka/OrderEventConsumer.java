dfa5f01adcedf0ddf8430ef633d390d9f4fcf262
XYSS\sytra <sytranvan2205@gmail.com>
2025-06-27 16:04:33 +0700

    Push code notification-service

--- notification-service/src/main/java/org/example/notificationservice/kafka/OrderEventConsumer.java
+++ notification-service/src/main/java/org/example/notificationservice/kafka/OrderEventConsumer.java
@@ -1,9 +1,28 @@
 package org.example.notificationservice.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventConsumer {
+    @Autowired
+    private JavaMailSender mailSender;
 
+    @KafkaListener(topics = "orders", groupId = "notify-group")
+    public void listen(String message) {
+        System.out.println("Received order message: " + message);
+        sendNotificationEmail(message);
+    }
+
+    public void sendNotificationEmail(String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("sytranvan2205@gmail.com");
+        message.setTo("sydeptrai92@gmail.com");
+        message.setSubject("New Order Received");
+        message.setText("Order content:\n" + content);
+        mailSender.send(message);
+    }
 }
